11fafc38ea72332eaa19e86fe18e06f35c3873c7
AzzaSalahEldin <azzasalaheldin11@gmail.com>
2025-06-04 17:46:52 +0300

    chage method names

--- BostaTask/src/test/java/BostaTest.java
+++ BostaTask/src/test/java/BostaTest.java
@@ -1,19 +1,14 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 public class BostaTest {
     private final String BASE_URI = "https://stg-app.bosta.co";
-    private final String TOKEN = "Bearer " + System.getenv("BOSTA_API_TOKEN");
-
-    @Test
-    public void testXSSInContactName() {
-        RestAssured.baseURI = BASE_URI;
-        System.out.println(TOKEN);
-
-        String body = """
+    private final String TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik1uN3FpVk9jQnFiWlNaTUFwbzFMdyIsInJvbGVzIjpbIkJVU0lORVNTX0FETUlOIl0sImJ1c2luZXNzQWRtaW5JbmZvIjp7ImJ1c2luZXNzSWQiOiJBWG5keU5kbXh6V2EweHVFdVJycmIiLCJidXNpbmVzc05hbWUiOiLYqtiz2Kog2KjYstmG2LMifSwia291bnRyeSI6eyJfaWQiOiI2MGU0NDgyYzdjYjdkNGJjNDg0OWM0ZDUiLCJuYW1lIjoiRWd5cHQiLCJuYW1lQXIiOiLZhdi12LEiLCJjb2RlIjoiRUcifSwiaW1haWwiOiJtb2hhbWVkLmVsa2hvbGFleSsxQGJvc3RhLmNvIiwicGhvbmUiOiIrMjAxMjAyNDM2MzA5IiwiZ3JvdXAiOnsiX2lkIjoiWGFxbENGQSIiLCJuYW1lIjoiQk9VSU5FU1NfRlVMTF9BQ0NFU1MiLCJjb2RlIjoxMTV9LCJ0b2tlblR5cGUiOiJBQ0NFU1MiLCJ0b2tlblZlcnNpb24iOiJWMiIsInNlc3Npb25JZCI6IjAxSldKSzQ4M0FTRjg0NjZLRFhQWVpGTVQyIiwiaWF0IjoxNzQ0NzE1MjYwLCJleHAiOjE3NDU5MjQ4NjB9.jjFo0wZ1Dx8_mpA2tFPD3bsqVClRVxo4CWnFS2E3tpg";
+    String body = """
         {
             "businessLocationId": "Q1MCImvcs",
             "contactPerson": {
@@ -30,7 +25,9 @@ public class BostaTest {
             "creationSrc": "Web"
         }
         """;
-
+    @Test
+    public void pickupWithValidToken() {
+        RestAssured.baseURI = BASE_URI;
         given()
                 .header("Authorization", TOKEN)
                 .contentType(ContentType.JSON)
@@ -38,7 +35,23 @@ public class BostaTest {
                 .when()
                 .post("/api/v2/pickups")
                 .then()
-                .statusCode(not(500))
-                .body("message", not(containsString("unexpected")));
+                .statusCode(200)
+                .log().all()
+                .extract().response();
     }
+    @Test
+    public void pickupWithInValidToken() {
+        RestAssured.baseURI = BASE_URI;
+        given()
+                .basePath("/api/v2/pickups")
+                .header("Authorization", "Bearer invalidtoken123")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(body)
+                .when()
+                .post()
+                .then()
+                .statusCode(401); // Unauthorized
+    }
+
 }
